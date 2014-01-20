91d8363a9dded86bc4f722f3c06be56f9201f7ae
niofis <niofis@gmail.com>
2014-01-19 23:22:39 -0700

    Working on file uploads
    Upgraded XPSeed

--- controllers/api/media.js
+++ controllers/api/media.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var multiparty = require('multiparty');
+var util = require('util');
+var fs = require('fs');
+var nosql = require('nosql').load('./db/media.nosql');
 var ctrl = require('../controller').create();
 exports.controller=ctrl.controller;
 
@@ -8,23 +12,39 @@ ctrl.map('GET','index',function (req,res,next){
 },'public');
 
 ctrl.map('POST','play',function (req,res,next){
-	console.log(req)
-	if(req.files && req.files.file){
-		console.log(req.files.file.path);
-		fs.readFile(req.files.file.path, function (err, data) {
+	/*
+	if(req.files && req.files.media){
+		fs.readFile(req.files.media.path, function (err, data) {
 			if(!err){
 				if(data){
-					
+					nosql.insert({cosa:1});
+					res.redirect('/');
 				} else {
 					ctrl.serverError(res);
 				}
 			} else {
 				ctrl.serverError(res);
 			}
-			fs.unlink(req.files.file.path);
+			fs.unlink(req.files.media.path);
 		});
 	}
 	else {
 		ctrl.serverError(res);
 	}
+	*/
+	var form = new multiparty.Form();
+
+	form.on('part', function(part){
+		fs.writeFileSync('./db/testfile',part);
+	})
+
+    form.parse(req, function(err, fields, files) {
+    	if(files && files.media){
+
+    	}
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+
 },'public');
