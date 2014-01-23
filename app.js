2a84e0f5c35fee856460408c82b6965ef4f75050
Enrique <niofis@gmail.com>
2014-01-23 12:36:19 -0700

    File size limits
    Place holders for folders.

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.engine('html', function(path,options,fn){
 });
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
